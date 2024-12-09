8777959398d56fa07241c2ba3124a8f449818fc9
Alejandro Salinas <121150895+uaposalinas@users.noreply.github.com>
2024-12-08 23:53:40 -0600

    IssueResolve

--- Vendor/main.request.js
+++ Vendor/main.request.js
@@ -119,6 +119,7 @@ function SetLoginService(ServiceKey){
             SensePath.innerHTML = `Iniciar Sesión <ion-icon name="chevron-forward-outline"></ion-icon> ${Name}`
             ServiceIdentifer.style.backgroundImage = `url(${Logo})`;
             TermsTitle.innerHTML = `¿Hello ID puede compartir datos con ${Name}?`;
+            TypeDisclamer()
 
         }else if(QueryResults.access == "false"){
 
@@ -198,8 +199,12 @@ function ChangeTheAccount(){
 }
 
 
-const DisclamerInfo = document.querySelector('.DisclamerInfo');
+function TypeDisclamer(){
+
+    const DisclamerInfo = document.querySelector('.DisclamerInfo');
 DisclamerInfo.innerHTML = `Si continúas, Hello ID compartirá tu nombre, tu dirección de correo electrónico, tu preferencia de idioma y tu imagen de perfil con ${localStorage.getItem('ServiceName')}. Consulta la <a href="/politica-privacidad" target="_blank">Política de Privacidad</a> y los <a href="/terminos-condiciones" target="_blank">Términos del Servicio</a> de este servicio.`
+
+}
 
 const Approve = document.querySelector('.Approve');
 const NoApprove = document.querySelector('.NoApprove');
